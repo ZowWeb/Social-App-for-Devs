1e25c2c88a7072c844f7e1b53eb55e4f64fe16a9
Zohaib Khan <zohaibandkhan@gmail.com>
2020-01-22 13:44:54 +0530

    Deploying to heroku

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
 	return (
 		<footer className="bg-dark text-white mt-5 p-4 text-center">
-			Copyright &copy; {new Date().getFullYear()} Connect Devs
+			Copyright &copy; {new Date().getFullYear()} DevConnector
 		</footer>
 	)
 }
